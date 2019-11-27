1195bc768446c7ebeb13cc60ab03ddc62fc11250
WavesT1 <jcotellesso@gmail.com>
2019-11-27 15:46:13 -0500

    Moved cake to own file.

--- Cake.py
+++ Cake.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env micropython
+
+def Cake (): #Fourth program takes the cake to a circle
+    #Waits for us to move our hands
+    time.sleep(0.5)
+    #Drives foward to put Cake in a circle
+    DriveBase.on_for_rotations(steering=0,speed=30,rotations=2.25)
+    #Drives back to home
+    DriveBase.on_for_rotations(steering=-10,speed=50,rotations=-6) 
--- WCSPV2.py
+++ WCSPV2.py
@@ -122,13 +122,6 @@ def Alift (): #Third program does Traffic jam
    Pusher.on_for_degrees(speed=100,degrees=-300)
    Arm.on_for_degrees(speed=-5,degrees=90,block=False)
    DriveBase.on_for_rotations(steering=0,speed=-90,rotations=5.5)
-def Cake (): #Fourth program takes the cake to a circle
-    #Waits for us to move our hands
-    time.sleep(0.5)
-    #Drives foward to put Cake in a circle
-    DriveBase.on_for_rotations(steering=0,speed=30,rotations=2.25)
-    #Drives back to home
-    DriveBase.on_for_rotations(steering=-10,speed=50,rotations=-6) 
 def TheStick(): #The last program which does swing, safety factor, and elevator
     #Makes sure rotator is in right spot
     Pusher.run_direct(duty_cycle_sp = 30)
